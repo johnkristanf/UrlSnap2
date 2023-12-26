8d909ccc1bf30f092cf0526a27d23dada5c6c6e4
John Kristan <jkpogz09@gmail.com>
2023-12-26 16:33:11 +0800

    refactor: added the useinterceptors cacheinterceptor for caching logic

--- app/server/src/modules/convert/convert.controller.ts
+++ app/server/src/modules/convert/convert.controller.ts
@@ -1,31 +1,30 @@
-import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
-import { ConvertService } from './convert.service';
+import { Body, Controller, Param, Post, Get, Res, UseInterceptors } from '@nestjs/common';
+import { CacheInterceptor } from '@nestjs/cache-manager';
 
+import { ConvertService } from './convert.service';
 import { convertTypes } from 'src/utils/types/convert';
+
 import { Response } from 'express';
 
 @Controller('convert')
 export class ConvertController {
   constructor(private readonly convertService: ConvertService) {}
 
+
   @Post()
-  async convertYTURLToMp3(@Body() data: convertTypes){
-    return this.convertService.convert(data.youtubeURL);
+  async convertYTURLToMp3(@Body() data: convertTypes, @Res() res : Response){
+    return this.convertService.convert(data.youtubeURL, res);
 
   }
 
 
-  @Get(':filePath')
-  downloadAudio(@Param('filePath') filePath: string, @Res() res: Response){
-    return this.convertService.download(filePath, res)
-    
+  @Get('title/:ytUrl')
+  @UseInterceptors(CacheInterceptor)
+  async getAudioTitle(@Param('ytUrl') ytUrl: string) {
+    return this.convertService.mp3Title(ytUrl);
   }
 
   
-  @Get('next/:filePath')
-  convertNext(@Param('filePath') filePath: string){
-    return this.convertService.next(filePath)
-    
-  }
+  
 
 }
