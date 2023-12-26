c52026cce80b0a8565cd7f5615b5fd733b7c48c1
John Kristan <jkpogz09@gmail.com>
2023-12-26 16:23:54 +0800

    build: Implement download http request

--- app/client/src/services/httpRequest/converter/download.ts
+++ app/client/src/services/httpRequest/converter/download.ts
@@ -1,31 +1,17 @@
 
-import axios from 'axios';
 
-
-export const downloadAudio = async (audioFilePath: string) => {
-
-    try {
-
-    const response =  await axios.get(`https://u-s-9rav.onrender.com/convert/${encodeURIComponent(audioFilePath)}`, {
-        responseType: 'arraybuffer'
-    });
-
-        const splitPath = audioFilePath.split('/');
-        const audioTittle = splitPath[ splitPath.length - 1 ];
-
-        const blob = new Blob([response.data], { type: 'audio/mpeg' });
-
-        const a = document.createElement('a');
-
-        a.href = URL.createObjectURL(blob);
-        a.download = audioTittle;
-
-        a.click();
-
-        URL.revokeObjectURL(a.href);
-
-    } catch (error) {
-      console.error('Error fetching QR code image:', error);
-    }
-  };
+export const downloadAudio = async (mp3Title:string, convertedAudioBuffer: any) => {
   
+  const blob = new Blob([convertedAudioBuffer], { type: 'audio/mpeg' });
+
+  const a = document.createElement('a');
+
+  a.href = URL.createObjectURL(blob);
+
+  a.download = `${mp3Title}.mp3`;
+
+  a.click();
+
+  URL.revokeObjectURL(a.href);
+  
+};
