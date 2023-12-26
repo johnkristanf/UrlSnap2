a541e96e5d083e1e56cb12731710954b3e047fe8
John Kristan <jkpogz09@gmail.com>
2023-12-26 16:34:28 +0800

    refactor: added the cachemodule and implementing the payloadlogger config

--- app/server/src/modules/convert/convert.module.ts
+++ app/server/src/modules/convert/convert.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
+
 import { ConvertService } from './convert.service';
 import { ConvertController } from './convert.controller';
 
+import { PayloadLoggerMiddleware } from 'src/middlewares/payload.middlware';
+
+
 @Module({
+  imports: [CacheModule.register()],
   controllers: [ConvertController],
   providers: [ConvertService],
 })
-export class ConvertModule {}
+
+export class ConvertModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(PayloadLoggerMiddleware).forRoutes('*');
+  }
+}
