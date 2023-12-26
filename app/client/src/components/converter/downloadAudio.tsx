e13b0c183b8df514008bf877b7200aeb42d1fc07
John Kristan <jkpogz09@gmail.com>
2023-12-26 16:12:15 +0800

    build: Implement the fetching mp3 title logic

--- app/client/src/components/converter/downloadAudio.tsx
+++ app/client/src/components/converter/downloadAudio.tsx
@@ -1,23 +1,45 @@
+
 import { DownloadAudioBtn, ConvertNextBtn } from "../ui/button"
 
-export const DownloadAudio = ({ setaudioFilePath, audioFilePath }: any) => {
+import { getMp3Title } from "../../services/httpRequest/converter/mp3Tittle"
+import { useEffect, useState } from "react";
+import { Loader } from "../loader";
 
-    const splitPathArray = audioFilePath.split('/');
+const DownloadAudio = ({ YTurl, setconvertedAudio, convertedAudio }: any) => {
 
-    const audioTittle = splitPathArray[splitPathArray.length - 1];
+    
+    console.log('convertedAudio', convertedAudio);
+
+    const [mp3Title, setmp3Title] = useState<string>();
+
+    useEffect(() => {
+
+        const getTittle = async () => {
+            setmp3Title(await getMp3Title(YTurl))
+        }
+
+        getTittle();
+
+    }, [YTurl]);
+
+    
+    if(!mp3Title) return <Loader />
+
 
     return(
 
         <div className="max-lg:w-[60%] max-sm:w-[80%] w-[40%] h-[90%] p-4 mt-8 text-white bg-violet-700 rounded-md">
 
-            <h1 className="font-semibold">{ audioTittle }</h1>
+            <h1 className="font-semibold">{ mp3Title } </h1>
 
             <div className="flex mt-4 gap-7">
-                <DownloadAudioBtn audioFilePath={audioFilePath} />
-                <ConvertNextBtn setaudioFilePath={setaudioFilePath} audioFilePath={audioFilePath} />
+                <DownloadAudioBtn mp3Title={mp3Title} convertedAudio={convertedAudio} />
+                <ConvertNextBtn setconvertedAudio={setconvertedAudio} />
             </div>
            
 
         </div>
     )
 }
+
+export default DownloadAudio
