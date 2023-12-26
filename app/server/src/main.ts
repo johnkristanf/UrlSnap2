8d628b4da7158de5aad152f7225a54f1bf0b75f9
John Kristan <jkpogz09@gmail.com>
2023-12-26 16:31:55 +0800

    refactor: changes the cors origin and the bodyparser limit to 100mb

--- app/server/src/main.ts
+++ app/server/src/main.ts
@@ -3,22 +3,27 @@ import { AppModule } from './app.module';
 
 import * as dotenv from 'dotenv';
 
+import * as bodyParser from 'body-parser'
+
+console.log('db', process.env.MONGODB_URI)
 
 async function bootstrap() {
   
-  const app = await NestFactory.create(AppModule,  {
-    
-    logger: ['error', 'debug'],
-    
-    cors: {
-      origin: 'https://urlsnap.vercel.app',
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-      allowedHeaders: ['Authorization', 'Content-Type', 'Content-Length', 'Content-Disposition']
-    }
-    
-  });
+  const app = await NestFactory.create(AppModule, { logger: ['debug', 'log', 'error'] });
 
-  const path = process.env.NODE_ENV === 'production' ? '../../.env.production' : '../.env';
+  app.enableCors({
+    origin: 'https://urlsnap.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Authorization', 'Content-Type', 'Content-Length', 'Content-Disposition'],
+
+  })
+
+  app.use(bodyParser.json({ limit: '100mb' }));
+  app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+
+ 
+ 
+  const path = process.env.NODE_ENV === 'production' ? '.env.production' : '.env';
     
   dotenv.config({ path: path });
 
